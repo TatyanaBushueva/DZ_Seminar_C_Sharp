50063a6c17f6c284e298214bd4b6117c2e2974a1
Ваше имя английскими буквами <>
2023-01-18 22:16:22 +0300

    Add Task25, Task27, Task29

---
+++ Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Ведите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB > 0)
+{
+    int degreeNumber = DegreeNumber(numberA, numberB);
+    Console.WriteLine($"{numberA} в степени {numberB} = {degreeNumber}");
+}
+else System.Console.WriteLine("Введеное число B не является натуральным");
+
+int DegreeNumber(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        {
+            result = result * num1;
+        }
+    }
+    return result;
+}
---
+++ Task27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Ведите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int sumNumber = SumNumber(number);
+    Console.WriteLine($"Сумма цифр в числе {number} = {sumNumber}");
+}
+else System.Console.WriteLine("Введено некоректное число");
+
+int SumNumber(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
